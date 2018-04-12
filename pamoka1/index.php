7f3b40d57567cec8833ba248c9fba1b569406fee
maryja <marija.nevdach@gmail.com>
2018-04-12 22:17:55 +0300

    First lesson

---
+++ pamoka1/index.php
@@ -0,0 +1,24 @@
+<?php 
+$type = "cat";
+$name = "Tim";
+$age = 1;
+$weight = 3.6;
+
+if ($type === "cat" && $name === "Tim"):
+    echo ("Tim is a cat");
+else: 
+    echo ("Tim is not a cat!");
+
+endif;
+
+echo (" ");
+
+$price = 582;
+
+if ($price > 500 && $price < 600):
+    echo ("Price is big enough");
+elseif ($price > 600 && $price < 700):
+    echo ("OMG");
+else:
+    echo ("Everything is OK");
+endif;
